5dc42fae816b496679ec3053c5046ea19c4b3500
cd19 <cd19@DESKTOP-F1G500K>
2023-10-03 13:23:58 -0700

    Agregar archivos de proyecto.

---
+++ Impreciones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPracticos1_JERH
+{
+    internal class Impreciones
+    {
+
+        public void ImPreciones(int[]numeros ) {
+            for(int X=0;X<5;X++ )
+            Console.WriteLine(numeros[X]);
+        }
+        public void Multiplos(int[] numeros,int multiplo)
+       {
+        
+            int[] num=new int[numeros.Length+1];
+            num=numeros;
+           int  multiplo2 = multiplo;
+            for (int X = 1; X < numeros.Length+1; X++)
+                num[X] = (multiplo2*(X));
+            ImprecionMultiplicacion(num);
+
+        }
+        public void ImprecionMultiplicacion(int[] numeros)
+        {
+            for (int X = 0; X < numeros.Length; X++)
+                Console.WriteLine(X);
+            
+        }
+
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EjerciciosPracticos1_JERH;
+Impreciones impre = new Impreciones();
+
+/*
+int[] numeros = new int[5];
+for (int X=0;X<numeros.Length;X++) {
+    Console.WriteLine($"Ingrese el Numero {X+1}");
+    numeros[X]=Convert.ToInt32(Console.ReadLine());
+}
+impre.ImPreciones(numeros);
+*/
+Console.WriteLine("================================");
+Console.WriteLine("INGRESE EL TAMAÑO DEL AREGLO");
+int tamaño= Convert.ToInt32(Console.ReadLine());
+int[] numeros2 = new int[tamaño];
+
+Console.WriteLine("ingrese el multiplo");
+int multiplo = Convert.ToInt32(Console.ReadLine());
+
+impre.Multiplos(numeros2, multiplo);
